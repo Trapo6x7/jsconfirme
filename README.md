7999bc299c58f7fd5154787fbc11985de901b958
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-15 13:56:50 +0100

    exo5.2

--- README.md
+++ README.md
@@ -23,7 +23,9 @@ Créer une boucle for qui génère une pyramide de * dans la console. Par exempl
 
 ## Exercice 2 : Boucle foreach
 
-Créer un tableau avec les noms de tes super-héros préférés. Utiliser une boucle foreach pour afficher une phrase pour chaque super-héros, par exemple "Je suis fan de [nom du super-héros]". Le résultat :
+Créer un tableau avec les noms de tes super-héros préférés. Utiliser une boucle foreach pour 
+afficher une phrase pour chaque super-héros, par exemple "Je suis fan de [nom du super-héros]".
+ Le résultat :
 
 ```
 Je suis fan de Spiderman
@@ -40,7 +42,9 @@ Créer une boucle while qui simule un compte à rebours de 10 à 0, puis affiche
 
 ## Exercice 4 : Boucles combinées
 
-Créer un tableau avec les noms de tes amis. Utilise une boucle for pour parcourir le tableau et une boucle while à l'intérieur de la boucle for pour afficher chaque nom autant de fois que sa position dans le tableau (en commençant à 1).
+Créer un tableau avec les noms de tes amis. Utilise une boucle for pour parcourir 
+le tableau et une boucle while à l'intérieur de la boucle for pour afficher chaque 
+nom autant de fois que sa position dans le tableau (en commençant à 1).
 
 Exemple :
 
--- script.js
+++ script.js
@@ -1,7 +1,131 @@
-let nombreLigne = parseInt(prompt("Donnez moi un nombre"))
+let iterations;
+let resultat = "";
 
-for (let i=0; i>=nombreLigne; i += 1){
-    let etoiles='*'.repeat(2 * i - 1);
-    console.log(etoiles);
+while (Number.isInteger(iterations) === false) {
+  iterations = Number(prompt("Veuillez saisir un nombre : "));
+}
+
+for (let i = 1; i <= iterations; i++) {
+  let star = "";
+
+  for (let x = 0; x < i; x++) {
+    star += "*";
+  }
+  resultat += star + "\n";
+}
+
+console.log(resultat);
+
+let height;
+let resultatSapin = "";
+
+while (Number.isInteger(height) === false) {
+  height = Number(prompt("Veuillez saisir un nombre : "));
+}
+
+for (let i = 0; i < height; i++) {
+  let line = "";
+  for (let j = 0; j < height - i - 1 / 2; j++) {
+    line += " ";
+  }
+  for (let j = 0; j < i + 1; j++) {
+    line += "*";
+  }
+  for (let j = 0; j < i - 1 / 2; j++) {
+    line += "*";
+  }
+
+  resultatSapin += line + "\n";
+}
+
+console.log(resultatSapin);
+
+const superHeros = [
+  "Spider-Man",
+  "Wonder Woman",
+  "Iron Man",
+  "Batman",
+  "Superman",
+  "Black Panther",
+  "Captain Marvel",
+  "Flash",
+  "Hulk",
+  "Thor",
+  "Green Lantern",
+  "Aquaman",
+  "Doctor Strange",
+  "Scarlet Witch",
+  "Black Widow",
+];
+
+superHeros.forEach((heros) => {
+  console.log("Je suis fan de " + heros);
+});
+
+let count = 10;
+
+do {
+  window.alert(count);
+  count -= 1;
+} while (count >= 0);
+
+if (count < 0) {
+  window.alert("Décolage!");
+}
+
+const prenoms = [
+  "Amina",
+  "Jamal",
+  "Mei",
+  "Ravi",
+  "Fatima",
+  "Omar",
+  "Sofia",
+  "Arjun",
+  "Leïla",
+  "Diego",
+];
+
+for (let i = 0; i < prenoms.length; i += 1) {
+  let ligne = "";
+  let count = 0;
+
+  while (count < i + 1) {
+    ligne += prenoms[i] + " ";
+    count += 1;
+  }
+
+  console.log(ligne.trim());
+}
+
+let trueFalse;
+
+while (Number.isInteger(trueFalse) === false) {
+  trueFalse = Number(prompt("Saisis un nombre entre 1 et 3"));
+}
+
+while (trueFalse != 3) {
+  trueFalse = Number(prompt("Essayes encore :("));
+}
+
+if (trueFalse === 3) {
+  alert("Bravo :)");
+}
+
+let trueFalse2;
+
+while (Number.isInteger(trueFalse2) === false) {
+  trueFalse2 = Number(prompt("Saisis un nombre"));
+}
+
+while (trueFalse2 != 14) {
+  trueFalse2 = Number(prompt("Essayes encore :("));
+    if (trueFalse2 === 14) {
+    alert("Bravo :)");
+  } else if (trueFalse2 < 10) {
+    alert("Plus grand!");
+  } else if (trueFalse2 > 20) {
+    alert("Plus petit!");
+  }
 }
 
